a81bca22d9a7c8d887370bcf8dfcc714c8861f28
Shivapriya-24 <shivapriya426@gmail.com>
2025-06-24 18:03:38 +0530

    product image 2

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/index', methods=['GET', 'POST'])
-def index():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         return redirect(url_for('appointment'))
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/appointment')
 def appointment():
